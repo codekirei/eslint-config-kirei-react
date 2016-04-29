524581e79ee9cea83d8f4f1386c12eeec1ab0a41
Jacob Blakely <jacob@codekirei.com>
2016-04-29 16:11:40 -0700

    add jsx-a11y rules

---
+++ rules/jsx-a11y.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  plugins: [
+    'jsx-a11y',
+  ],
+  rules: {
+    'jsx-a11y/aria-props': 2,
+    'jsx-a11y/aria-proptypes': 2,
+    'jsx-a11y/aria-role': 2,
+    'jsx-a11y/aria-unsupported-elements': 2,
+    'jsx-a11y/href-no-hash': 0,
+    'jsx-a11y/img-has-alt': 2,
+    'jsx-a11y/img-redundant-alt': 2,
+    'jsx-a11y/label-has-for': 2,
+    'jsx-a11y/mouse-events-have-key-events': 2,
+    'jsx-a11y/no-access-key': 2,
+    'jsx-a11y/no-onchange': 2,
+    'jsx-a11y/onclick-has-focus': 2,
+    'jsx-a11y/onclick-has-role': 2,
+    'jsx-a11y/role-has-required-aria-props': 2,
+    'jsx-a11y/role-supports-aria-props': 2,
+    'jsx-a11y/tabindex-no-positive': 0,
+  },
+}
