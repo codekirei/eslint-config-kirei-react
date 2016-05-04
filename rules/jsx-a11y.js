1f0f5343070c431150dc4a2ad9cb0d597433e5bd
Jacob Blakely <jacob@codekirei.com>
2016-05-03 21:03:58 -0700

    no-onchange -> 2, use onInput for <input>s

--- rules/jsx-a11y.js
+++ rules/jsx-a11y.js
@@ -11,7 +11,7 @@ module.exports = {
   'jsx-a11y/label-has-for': 2,
   'jsx-a11y/mouse-events-have-key-events': 2,
   'jsx-a11y/no-access-key': 2,
-  'jsx-a11y/no-onchange': 0,
+  'jsx-a11y/no-onchange': 2,
   'jsx-a11y/onclick-has-focus': 2,
   'jsx-a11y/onclick-has-role': 2,
   'jsx-a11y/role-has-required-aria-props': 2,
